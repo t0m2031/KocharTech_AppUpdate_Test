a3ba938fa12e2c12149b0cce099d05701c45b441
Shubhankar <shubhu311@gmail.com>
2024-04-17 19:30:45 +0530

    Updated configuration settings in client_config.py

--- client_config.py
+++ client_config.py
@@ -1,8 +1,11 @@
 from pyupdater.client import ClientConfig
 
+# client_config.py
 def get_client_config():
-    client_config = ClientConfig()
-    client_config.APP_NAME = "MyApp"
-    client_config.APP_VERSION = "1.0.0"
-    client_config.UPDATE_URLS = ["http://example.com/updates/"]
+    client_config = {
+        'APP_NAME': 'KocharTechApp',
+        'APP_VERSION': '1.0.0',
+        'UPDATE_URLS': ['https://github.com/t0m2031/KocharTech_AppUpdate_Test/releases']
+        # Add more configuration settings if needed
+    }
     return client_config
